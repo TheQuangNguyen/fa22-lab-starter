93861de577aff154b4ab79bc7af9d45e55f0f11e
Quang Nguyen <quangn2595@gmail.com>
2023-01-04 12:45:31 -0800

    Completed lab 1

--- lab01/exercise3/ll_cycle.c
+++ lab01/exercise3/ll_cycle.c
@@ -3,4 +3,18 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    node *slow_ptr = head;
+    node *fast_ptr = head;
+
+     do {
+        for (size_t i = 0; i < 2; i++) {
+            if (fast_ptr == NULL) {
+                return 0;
+            }
+            fast_ptr = fast_ptr->next;
+        }
+        slow_ptr = slow_ptr->next;
+    } while (fast_ptr != slow_ptr);
+
+    return 1;
 }
